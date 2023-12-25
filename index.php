0d19dece411750efee99ccede138078ea8d91833
Chenghuexin <cindy2090.cheng@gmail.com>
2023-12-25 14:59:49 +0800

    change this page's button. in this page, user can click detection button into detection page

--- index.php
+++ index.php
@@ -6,17 +6,16 @@
     <meta name="viewport" http-equiv="Content-Type" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel=stylesheet type="text/css" href="css/index.css">
-    <title>Skin Cancaer detection System</title>
+    <title>Skin Cancer detection System</title>
 </head>
 
 <body>
     <div id="header">
-        <h1>Skin Cancaer detection System</h1>
+        <h1>Skin Cancer detection System</h1>
     </div>
 
     <div>
-        <a href='instrcution.php' style="margin-right: 130px;"><button type="button" class="btn btn-warning">Instruction</button></a>
-        <a href='detection.php' style="margin-left: 130px;"><button type="button" class="btn btn-info">Detection</button></a>
+        <a href='detection.php'><button type="button" class="btn btn-info">Detection</button></a>
         <!-- <button type="button" class="btn btn-info" disabled style="margin-left: 130px;">Online Show</button> -->
     </div>
 
